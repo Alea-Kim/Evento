81cdd280937f594b2e4f44caf9e7098db8d91e8f
Patricia Caparoso <tricia.caparoso@gmail.com>
2015-12-17 12:20:53 +0800

    Nilakihan ung pics

--- 4. Website/cs165.php
+++ 4. Website/cs165.php
@@ -5,7 +5,6 @@
 		<script type="text/javascript" src="js/jssor.slider.min.js"></script>
     <script>
         jssor_1_slider_init = function() {
-
             var jssor_1_options = {
               $AutoPlay: true,
               $ArrowNavigatorOptions: {
@@ -13,7 +12,7 @@
               },
               $ThumbnailNavigatorOptions: {
                 $Class: $JssorThumbnailNavigator$,
-                $Cols: 4,
+                $Cols: 5,
                 $SpacingX: 4,
                 $SpacingY: 4,
                 $Orientation: 2,
@@ -28,11 +27,11 @@
             function ScaleSlider() {
                 var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
                 if (refSize) {
-                    refSize = Math.min(refSize, 810);
+                    refSize = Math.min(refSize, 900);
                     jssor_1_slider.$ScaleWidth(refSize);
                 }
                 else {
-                    window.setTimeout(ScaleSlider, 30);
+                    window.setTimeout(ScaleSlider, 40);
                 }
             }
             ScaleSlider();
@@ -41,37 +40,6 @@
             $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
         };
     </script>
-    <style>
-        .jssora02l, .jssora02r {
-            display: block;
-            position: absolute;
-            /* size of arrow element */
-            width: 55px;
-            height: 55px;
-            cursor: pointer;
-            background: url('img/a02.png') no-repeat;
-            overflow: hidden;
-        }
-        .jssora02l { background-position: -3px -33px; }
-        .jssora02r { background-position: -63px -33px; }
-        .jssora02l:hover { background-position: -123px -33px; }
-        .jssora02r:hover { background-position: -183px -33px; }
-        .jssora02l.jssora02ldn { background-position: -3px -33px; }
-        .jssora02r.jssora02rdn { background-position: -63px -33px; }
-				.jssort11 .p { position: absolute; top: 0; left: 0; width: 200px; height: 69px; background: #181818;}
-        .jssort11 .tp { position: absolute; top: 0; left: 0; width: 100%; height: 100%; border: none;}
-        .jssort11 .i, .jssort11 .pav:hover .i { position: absolute; top: 3px; left: 3px; width: 60px; height: 30px; border: white 1px dashed;}
-        * html .jssort11 .i { width /**/: 62px; height /**/: 32px;}
-        .jssort11 .pav .i { border: white 1px solid;}
-        .jssort11 .t, .jssort11 .pav:hover .t { position: absolute; top: 3px; left: 68px; width: 129px; height: 32px; line-height: 32px; text-align: center; color: #fc9835; font-size: 13px; font-weight: 700;}
-        .jssort11 .pav .t, .jssort11 .p:hover .t { color: #fff;}
-        .jssort11 .c, .jssort11 .pav:hover .c { position: absolute; top: 38px; left: 3px; width: 197px; height: 31px; line-height: 31px; color: #fff; font-size: 11px; font-weight: 400; overflow: hidden;}
-        .jssort11 .pav .c, .jssort11 .p:hover .c { color: #fc9835;}
-        .jssort11 .t, .jssort11 .c { transition: color 2s; -moz-transition: color 2s; -webkit-transition: color 2s; -o-transition: color 2s;}
-        .jssort11 .p:hover .t, .jssort11 .pav:hover .t, .jssort11 .p:hover .c, .jssort11 .pav:hover .c { transition: none; -moz-transition: none; -webkit-transition: none; -o-transition: none;}
-        .jssort11 .p:hover, .jssort11 .pav:hover { background: #333;}
-        .jssort11 .pav, .jssort11 .p.pdn { background: #462300;}
-    </style>
 	</head>
 	<body>
 		<nav>
@@ -83,16 +51,16 @@
 				<a href=""><img src="search.png" width="30px" height="30px"/></a>
 			</div>
 		</nav>
-		<div id="jssor_1">
-        <div data-u="loading" style="position: absolute; top: 0px; left: 0px;">
-            <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block; top: 0px; left: 0px; width: 100%; height: 100%;"></div>
-            <div style="position:absolute;display:block;background:url('img/loading.gif') no-repeat center center;top:0px;left:0px;width:100%;height:100%;"></div>
+		<div class="banner" id="jssor_1">
+        <div data-u="loading" style="position: absolute;">
+            <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block; width: 100%; height: 100%;"></div>
+            <div style="display:block;background:url('img/loading.gif') no-repeat center center;width:100%;height:100%;"></div>
         </div>
-        <div data-u="slides" style="cursor: default; position: relative; left: 20%; width: 300px; height: 400px; overflow: hidden;">
+        <div data-u="slides" style="cursor: default; left: 20%; width: 300px; height: 450px; overflow: hidden;">
             <div data-p="112.50" style="display: none;">
                 <img data-u="image" width="150px" height="480px" src="image1.jpg" />
                 <div data-u="thumb">
-                    <img class="i" src="img/thumb-002.jpg" />
+                    <img class="i" src="image1.jpg" />
                     <div class="t">Banner Rotator</div>
                     <div class="c">360+ touch swipe slideshow effects</div>
                 </div>
@@ -100,7 +68,7 @@
             <div data-p="112.50" style="display: none;">
                 <img data-u="image" width="150px" height="480px" src="image2.jpg" />
                 <div data-u="thumb">
-                    <img class="i" src="img/thumb-003.jpg" />
+                    <img class="i" src="image2.jpg" />
                     <div class="t">Image Gallery</div>
                     <div class="c">Image gallery with thumbnail navigation</div>
                 </div>
@@ -108,7 +76,7 @@
             <div data-p="112.50" style="display: none;">
                 <img data-u="image" width="150px" height="480px" src="image1.jpg" />
                 <div data-u="thumb">
-                    <img class="i" src="img/thumb-004.jpg" />
+                    <img class="i" src="image1.jpg" />
                     <div class="t">Carousel</div>
                     <div class="c">Touch swipe, mobile device optimized</div>
                 </div>
@@ -116,7 +84,7 @@
             <div data-p="112.50" style="display: none;">
                 <img data-u="image" width="150px" height="480px" src="image2.jpg" />
                 <div data-u="thumb">
-                    <img class="i" src="img/thumb-005.jpg" />
+                    <img class="i" src="image2.jpg" />
                     <div class="t">Themes</div>
                     <div class="c">30+ professional themems + growing</div>
                 </div>
@@ -124,23 +92,19 @@
             <div data-p="112.50" style="display: none;">
                 <img data-u="image" width="150px" height="480px" src="image1.jpg" />
                 <div data-u="thumb">
-                    <img class="i" src="img/thumb-006.jpg" />
+                    <img class="i" src="image1.jpg" />
                     <div class="t">Tab Slider</div>
                     <div class="c">Tab slider with auto play options</div>
                 </div>
             </div>
         </div>
-        <!-- Thumbnail Navigator -->
-        <div data-u="thumbnavigator" class="jssort11" style="position:absolute;right:5px;top:0px;font-family:Arial, Helvetica, sans-serif;-moz-user-select:none;-webkit-user-select:none;-ms-user-select:none;user-select:none;width:200px;height:300px;" data-autocenter="2">
-            <!-- Thumbnail Item Skin Begin -->
+        <div data-u="thumbnavigator" class="jssort11" data-autocenter="2">
             <div data-u="slides" style="cursor: default;">
                 <div data-u="prototype" class="p">
                     <div data-u="thumbnailtemplate" class="tp"></div>
                 </div>
             </div>
-            <!-- Thumbnail Item Skin End -->
         </div>
-        <!-- Arrow Navigator -->
         <span data-u="arrowleft" class="jssora02l" style="top:0px;left:8px;width:55px;height:55px;" data-autocenter="2"></span>
         <span data-u="arrowright" class="jssora02r" style="top:0px;right:218px;width:55px;height:55px;" data-autocenter="2"></span>
         <a href="http://www.jssor.com" style="display:none">Bootstrap Carousel</a>
